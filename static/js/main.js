365fa24076860fdbca2de05864995149bb1d0ea5
rafcevallos <rafael.cevallos81@gmail.com>
2018-08-27 15:52:59 -0500

    Printing data to DOM achieved! Sorta because it's still tied to a button

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,3 @@
-console.log("I am here!!")
-
 document.getElementById("js-clicker").addEventListener("click", () => {
     fetch('http://127.0.0.1:8000/birthday')
         .then(response => {
@@ -8,9 +6,11 @@ document.getElementById("js-clicker").addEventListener("click", () => {
         .then(birthdays => {
             console.log(JSON.stringify(birthdays))
             console.log("%cYAY!🖌🎨🖼", "color: red; font-family: sans-serif; font-size: 4.5em; font-weight: bolder; text-shadow: #000 1px 1px;") /* stylize the console log message */
+            const bd = document.getElementById("birthday-list")
+            birthdays.forEach(birthday => {
+                let blist = document.createElement("li")
+                blist.textContent = `Happy Birthday, ${birthday.name}!  ${birthday.greeting}`
+                bd.appendChild(blist)
+            })
         })
 })
-
-// TODO: Make an http request to ""
-
-// url: "/birthdays"
